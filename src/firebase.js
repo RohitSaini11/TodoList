9580b6dd6915ea8be366ccf74de9d1a42339cf53
Rohit Saini <rs0118032@gmail.com>
2023-07-03 16:17:35 +0530

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwl6p1Jcu6qHa-3J2PAPFWop8gxpb5y64",
+  authDomain: "todolist-d6937.firebaseapp.com",
+  projectId: "todolist-d6937",
+  storageBucket: "todolist-d6937.appspot.com",
+  messagingSenderId: "1058715169966",
+  appId: "1:1058715169966:web:dc522e0c6052213182293f"
+};
+
+const app = initializeApp(firebaseConfig);
